eda69c34896420dffbfa83950911cf21f9beb5c9
Mehmet Atılgan <127437116+mehmetatilgan@users.noreply.github.com>
2023-03-28 23:47:48 +0300

    Add files via upload

--- DotsAndBoxes.py
+++ DotsAndBoxes.py
@@ -1,44 +1,85 @@
-a = int(input("sayı:"))
-b = int(input("sayı:"))
-
-
-def drawLine(x1,x2,y1,y2, board):  # 1,1 2,1 / 1,1 1,2
+def drawLine(x1, x2, y1, y2, board):
     if x1 == y1:
-        board[x1+(x1-1)] = board[x1+(x1-1)][:x2+(x2-1) + 3] + "—" + board[x1+(x1-1)][x2+(x2-1) + 4:]
-
+        z = min(x2, y2)
+        board[2 * x1 - 1] = board[2 * x1 - 1][:2 * z + 2] + "—" + board[2 * x1 - 1][2 * z + 3:]
     elif x2 == y2:
-        board[y1+(y1-2)] = board[y1+(y1-2)][:x2+(x2-2) + 3] + "│" + board[y1+(y1-2)][x2+(x2-2) + 4:]
-
+        z = max(x1, y1)
+        board[2 * z - 2] = board[2 * z - 2][:2 * x2 + 1] + "│" + board[2 * z - 2][2 * x2 + 2:]
     else:
         print("Wrong input")
 
-def controlSquare():
-    print("Square")
+
+def controlSquare(x1, x2, y1, y2, board, currentPlayer):
+    if x1 == y1:
+        if "│" in board[2 * x1 - 2][2 * x2 + 1] and "│" in board[2 * x1 - 2][2 * x2 + 3] and "—" in board[2 * x1 - 3]:
+            board[2 * x1 - 2] = board[2 * x1 - 2][:2 * x2 + 2] + currentPlayer[0] + board[2 * x1 - 2][2 * x2 + 3:]
+        if "│" in board[x1 * 2][2 * x2 + 1] and "│" in board[x1 * 2][2 * x2 + 3] and "—" in board[2 * x1 - 1]:
+            board[x1 * 2] = board[x1 * 2][:2 * x2 + 2] + currentPlayer[0] + board[x1 * 2][2 * x2 + 3:]
+    elif x2 == y2:  # 1 1 2 1 /2 1 3 1/ 3 2 4 2
+        z = max(x1, y1)
+        if "—" in board[2 * z - 1][2 * x2 + 2] and "—" in board[2 * z - 3][2 * x2 + 2] and "│" in \
+                board[2 * z - 2][2 * x2 + 3]:
+            board[2 * z - 2] = board[2 * z - 2][:2 * x2 + 2] + currentPlayer[0] + board[2 * z - 2][2 * x2 + 3:]
+        if "—" in board[2 * z - 1][x2 * 2] and "—" in board[2 * z - 3][x2 * 2] and "│" in board[2 * z - 2][2 * x2 + 1]:
+            board[2 * z - 2] = board[2 * z - 2][:2 * x2] + currentPlayer[0] + board[2 * z - 2][2 * x2 + 1:]
+    else:
+        print("Wrong input")
 
 
-
-def printBoard():
+def printBoard(board):
     for k in range(len(board)):
         if k != len(board) - 2:
             print(board[k])
 
 
-board = ["---" * a]
-for i in range(b):
-    board.append(str(i + 1) + "| " + "∙ " * a + " |")
-    board.append("   " + " " * (2 * a) + "  ")
-board.append("---" * a)
+def startGame():
+    print("**************************************************\n\nWelcome to DotsAndBoxes "
+          "Game\n\n**************************************************\n\nRules:The two players take turns to join two "
+          "adjacent dots with a horizontal or vertical line.\nIf a player completes the fourth side of a box they initial "
+          "that box and must draw another line.\nWhen all the boxes have been completed the winner is the player who has "
+          "initialled the most boxes.")
+    player1 = input("First Player's name: ")
+    player2 = input("Second Player's name: ")
+    row = int(input("Board's row:"))
+    column = int(input("Board's column:"))
 
-drawLine(1,1,1,2,board)
-drawLine(1,1,2,1,board)
-drawLine(2,1,2,2,board)
-drawLine(2,1,3,1,board)
-drawLine(4,4,4,5,board)
-drawLine(3,3,3,4,board)
-drawLine(3,4,4,4,board)
-drawLine(3,5,4,5,board)
-drawLine(3,4,3,5,board)
-drawLine(2,5,2,6,board)
-printBoard()
+    maxSquare = (row - 1) * (column - 1) / 2
+    currentPlayer = player1
+    playerOnePoints = 0
+    playerTwoPoints = 0
 
-print(len(board[0]))
+    a = 0
+    m = a
+
+    board = ["———" * column]
+    for i in range(row):
+        board.append(str(i + 1) + "│ " + "∙ " * column + " │")
+        board.append(" │ " + " " * (2 * column) + " │")
+    board.append("———" * column)
+
+    printBoard(board)
+
+    while (playerTwoPoints or playerOnePoints) <= maxSquare:
+        a = 0
+        print(currentPlayer + "'s Turn")
+        x1, x2, y1, y2 = list(input("Değeleri giriniz:").split())
+        drawLine(int(x1), int(x2), int(y1), int(y2), board)
+        controlSquare(int(x1), int(x2), int(y1), int(y2), board, currentPlayer)
+        printBoard(board)
+
+        for row in board:
+            for index in row:
+                if index == currentPlayer[0]:
+                    a += 1
+        print(currentPlayer + "'s total points:", a)
+
+        if m < a:
+            m = a
+        else:
+            if currentPlayer == player1:
+                currentPlayer = player2
+            else:
+                currentPlayer = player1
+
+
+startGame()
